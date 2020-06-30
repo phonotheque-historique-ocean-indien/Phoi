e91f42f8d16228589c58a0c33d96aab6883ec13d
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-30 13:11:57 +0200

    adding description

--- controllers/PartenairesController.php
+++ controllers/PartenairesController.php
@@ -52,13 +52,34 @@ La Réunion est envoûtante, multi-ethnique, authentique, sa culture se conjugue
                 $facet_id = 1286;
                 break;
             case "seychelles":
-                $description = "...";
+                $description = "Les Seychelles forment un archipel de 115 îles (dont une artificielle), situé dans l'ouest de l'océan Indien et rattaché au continent africain. Toutes ces îles sont regroupées en un État dont la capitale est la ville de Victoria sur l'île principale de Mahé.";
                 $facet_id = 1016;
                 break;
             case "maurice":
-                $description = "…";
+                $description = "Maurice (en anglais : Mauritius), en forme longue la république de Maurice (en anglais : Republic of Mauritius), est un État insulaire de l'océan Indien à 868 kilomètres à l'est de Madagascar et 172 kilomètres à l'est-nord-est de La Réunion. Le pays inclut l'île principale de Maurice, mais aussi l'île Rodrigues à 560 kilomètres à l'est de l'île principale. Les îles plus lointaines d'Agaléga et de Saint-Brandon font partie du territoire national. Les îles Maurice et Rodrigues font partie de l'archipel des Mascareignes, avec l'île de La Réunion qui est elle un département d'outre-mer français. La capitale et plus grande ville est Port-Louis.";
                 $facet_id = 1064;
                 break;
+            case "rodrigues":
+                $description = "Rodrigues est la plus petite des îles principales de l’archipel des Mascareignes. Elle est surnommée la Cendrillon des Mascareignes. D'origine volcanique, l'île se situe à 583 km à l’est de Maurice, presque isolée au centre de l’océan Indien. D’une superficie de 109 km2, elle mesure 18 km de long sur 8 de large et présente la particularité d’avoir un lagon d’une surface deux fois supérieure à celle des terres émergées. Elle fait partie de la république de Maurice et jouit d'un statut d'autonomie depuis le 12 octobre 2002.";
+                $facet_id = 1064;
+                break;
+            case "mayotte":
+                $description = "Mayotte (en mahorais : Maoré), officiellement nommée département de Mayotte, est à la fois une région insulaire française et un département de France d'outre-mer qui sont administrés dans le cadre d’une collectivité territoriale unique4 dirigée par le conseil départemental de Mayotte. Sur le plan géographique, il s’agit d’un ensemble d’îles situé dans le canal du Mozambique et dans l’océan Indien. Mayotte est constituée de deux îles principales, Grande-Terre et Petite-Terre, et de plusieurs autres petites îles dont Mtsamboro, Mbouzi et Bandrélé. Son code départemental officiel est « 976 »5. L’ancien chef-lieu Dzaoudzi est situé en Petite-Terre. Le chef-lieu actuel de jure est Mamoudzou6 sur Grand-Terre, le siège du Conseil départemental et les services administratifs de la préfecture sont tous deux sur Grande-Terre à Mamoudzou, ville la plus peuplée de Mayotte. Du fait de son statut de région française, Mayotte est également une région ultrapériphérique de l’Union européenne.";
+                $facet_id = 1064;
+                break;
+            case "comores":
+                $description = "Les Comores, en forme longue l'union des Comores (en comorien : Komori et Udzima wa Komori, en arabe : جزر القمر (Djuzur al qamar) et الاتّحاد القُمُريّ (al-Ittiḥād al-Qumuriyy)), est une république fédérale d'Afrique australe située dans le nord du canal du Mozambique, un espace maritime de l'océan Indien. Le pays a pour capitale Moroni, pour langues officielles le comorien (shikomor), parlé par 96,9 % de la population5, le français et l'arabe6,7 et pour monnaie le franc comorien. L'Union des Comores est membre de la Ligue arabe, de l'Organisation de la coopération islamique, de l'Organisation internationale de la francophonie et de l'Assemblée parlementaire de la francophonie. Colonie française à partir de 1892, les Comores obtiennent leur indépendance le 6 juillet 1975.";
+                $facet_id = 1064;
+                break;
+            case "zanzibar":
+                $description = "Zanzibar, en swahili Funguvisiwa ya Zanzibar, est un archipel de l'océan Indien situé en face des côtes tanzaniennes, formé de trois îles principales (Unguja, Pemba et Mafia) et de plusieurs autres petites îles. Les îles d'Unguja et de Pemba forment depuis plusieurs siècles une entité tour à tour indépendante (sultanat de Zanzibar et État de Zanzibar), colonisée par le Royaume-Uni (protectorat de Zanzibar) ou incorporée à la Tanzanie (Gouvernement révolutionnaire de Zanzibar). L'île de Mafia a quant à elle toujours été intégrée à la Tanzanie continentale (Afrique orientale allemande, territoire sous mandat de Tanganyika et État de Tanganyika).";
+                $facet_id = 1064;
+                break;
+            case "madagascar":
+                $description = "Madagascar, en forme longue la République de Madagascar (en malgache : Madagasikara, ou Repoblikan'i Madagasikara), est un État insulaire situé dans l'océan Indien et géographiquement rattaché au continent africain, dont il est séparé par le canal du Mozambique. C’est la cinquième plus grande île du monde après l'Australie, le Groenland, la Nouvelle-Guinée et Bornéo. Longue de 1 580 km et large de 580 km, Madagascar couvre une superficie de 587 000 km2. Sa capitale est Antananarivo6 et le pays a pour monnaie l'ariary. Ses habitants, les Malgaches, sont un peuple austronésien parlant une langue malayo-polynésienne : le malgache. Le pays est entouré par d'autres îles et archipels dont l'île Maurice, les Seychelles, Mayotte, les Comores et La Réunion.";
+                $facet_id = 1064;
+                break;
+
         }
         $this->view->setVar("pays", $pays);
         $this->view->setVar("description", $description);
