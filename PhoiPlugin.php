84e2a62d97f24e583316eb2ea949d7a0d58fb8bd
Gautier Michelin <gautier.michelin@gmail.com>
2020-06-30 12:45:14 +0200

    main plugin file

---
+++ PhoiPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+class PhoiPlugin extends BaseApplicationPlugin {
+    # -------------------------------------------------------
+    protected $description = 'Articles for CollectiveAccess Pawtucket';
+    # -------------------------------------------------------
+    private $opo_config;
+    private $ops_plugin_path;
+    # -------------------------------------------------------
+    public function __construct($ps_plugin_path) {
+        $this->ops_plugin_path = $ps_plugin_path;
+        $this->description = _t('Articles plugin');
+        parent::__construct();
+        $this->opo_config = Configuration::load($ps_plugin_path.'/conf/phoi.conf');
+    }
+    # -------------------------------------------------------
+    /**
+     * Override checkStatus() to return true - the statisticsViewerPlugin always initializes ok... (part to complete)
+     */
+    public function checkStatus() {
+        return array(
+            'description' => $this->getDescription(),
+            'errors' => array(),
+            'warnings' => array(),
+            'available' => ((bool)$this->opo_config->get('enabled'))
+        );
+    }
+    # -------------------------------------------------------
+    /**
+     * Add plugin user actions
+     */
+    static function getRoleActionList() {
+        return array();
+    }
+    # -------------------------------------------------------
+}
+?>
