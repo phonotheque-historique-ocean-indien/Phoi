07d444f04f34cdc503850d166abbf09ee8615b74
Gautier Michelin <gautier.michelin@gmail.com>
2020-07-01 22:10:21 +0200

    users controller

---
+++ controllers/UsersController.php
@@ -0,0 +1,57 @@
+<?php
+
+ini_set("display_errors", 1);
+error_reporting(E_ERROR);
+require_once(__CA_MODELS_DIR__.'/ca_site_pages.php');
+require_once(__CA_LIB_DIR__.'/Search/ObjectSearch.php');
+require_once(__CA_LIB_DIR__.'/Browse/EntityBrowse.php');
+require_once(__CA_LIB_DIR__.'/Browse/ObjectRepresentationBrowse.php');
+
+class UsersController extends ActionController
+{
+    # -------------------------------------------------------
+    protected $opo_config;        // plugin configuration file
+    protected $opa_list_of_lists; // list of lists
+    protected $opa_listIdsFromIdno; // list of lists
+    protected $opa_locale; // locale id
+    private $opo_list;
+    private $plugin_path;
+
+    # -------------------------------------------------------
+    # Constructor
+    # -------------------------------------------------------
+
+    public function __construct(&$po_request, &$po_response, $pa_view_paths = null)
+    {
+        parent::__construct($po_request, $po_response, $pa_view_paths);
+		$this->plugin_path = __CA_APP_DIR__ . '/plugins/Phoi';
+
+        $this->opo_config = Configuration::load(__CA_APP_DIR__ . '/plugins/Phoi/conf/phoi.conf');
+
+		// Extracting theme name to properly handle views in distinct theme dirs
+        $vs_theme_dir = explode("/", $po_request->getThemeDirectoryPath());
+        $vs_theme = end($vs_theme_dir);
+        $this->opa_view_paths[] = $this->plugin_path."/themes/".$vs_theme."/views";
+    }
+
+    # -------------------------------------------------------
+    # Functions to render views
+    # -------------------------------------------------------
+
+    public function List() {
+        $o_data = new Db();
+        $qr_result = $o_data->query("
+            SELECT * 
+            FROM ca_users 
+        ");
+        $content="";
+        while($qr_result->nextRow()) {
+            $content .= "User : ".$qr_result->get('user_name')."<br/>\n";
+        }
+        $this->view->setVar("content", $content);
+        $this->render('users_list_html.php');
+        //exit();
+    }
+
+}
+?>
